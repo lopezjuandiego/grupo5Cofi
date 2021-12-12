f2cca06768f7d3bcac41a17b730cc59ceafdad92
Juan Diego López <lopezjuandiego@gmail.com>
2021-12-12 11:54:46 -0300

    agregué directorio en app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.set ("port", process.env.PORT || 3050);
-app.listen (app.get("port"), ()=>console.log("Server corriendo en puerto 3050 http://localhost:" + app.get("port")));
+app.set("port", process.env.PORT || 3050);
+app.listen(app.get("port"), () =>
+  console.log(
+    "Server corriendo en puerto 3050 http://localhost:" + app.get("port")
+  )
+);
 
-app.use (express.static(path.resolve(__dirname, "../public")))
-app.get('/', (req, res)  => res.sendFile(path.resolve (__dirname,'./views/home.html')))
-app.get('/home', (req, res)  => res.sendFile(path.resolve (__dirname,'./views/home.html')))
-app.get ("/productCart", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/productCart.html"))});
-app.get ("/productDetail", (req, res) =>{
-        res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))});
+app.use(express.static(path.resolve(__dirname, "../public")));
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/home.html"))
+);
+app.get("/home", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/home.html"))
+);
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
+});
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
+});
 
-
-
+app.get("/pagoyenvio", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/pagoyenvio.html"));
+});
