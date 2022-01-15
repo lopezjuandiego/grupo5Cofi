f0b3420c7b9048a6df45c80eee66a3232f5aee9e
Juan Diego López <lopezjuandiego@gmail.com>
2022-01-14 22:10:10 -0300

    funciona la carga al JSON

--- src/routes/product.js
+++ src/routes/product.js
@@ -3,10 +3,15 @@ const controllers = require('../controllers/product')
 const path = require ('path');
 const multer = require ('multer');
 const router = express.Router();
+const upload = multer({storage: multer.diskStorage({
+    destination: (req,file,cb) => (null, path.resolve (__dirname, '../../uploads')) ,
+    filename: (req, file, cb) => cb(null, file.fieldname + '-' + Date.now () + path.extname(file.originalname))
+})})
 
 router.get('/product', controllers.index)
 router.get('/create', controllers.create)
-router.get('/', controllers.save) 
+
+router.post('/product',[upload.any()], controllers.save) 
 
 
 
