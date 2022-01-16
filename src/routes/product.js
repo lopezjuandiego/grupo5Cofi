edcca3e62beb908e1d8940a5dedb7eb47c604d0f
mendoplayer <fernandocampos19888@gmail.com>
2022-01-16 20:16:46 -0300

    modificacion de rutas

--- src/routes/product.js
+++ src/routes/product.js
@@ -4,20 +4,15 @@ const path = require ('path');
 const multer = require ('multer');
 const router = express.Router();
  const upload = multer({storage:multer.diskStorage({
-    destination: (req,file,cb) => (null, path.resolve (__dirname, '../../uploads')),
+    destination: (req,file,cb) => cb(null, path.resolve (__dirname, '../../uploads')),
     filename: (req, file, cb) => cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
 })}) 
-/*const storage = multer.diskStorage({
-    destination: (req,file,cb) => (null, path.resolve (__dirname, '../../uploads')),
-    filename: (req, file, cb) => cb(null, file.fieldname + '-' + Date.now () + path.extname(file.originalname))
-})
 
-const upload = multer({storage:storage}) */
 
 router.get('/', controllers.index)
-router.get('/create', controllers.create)
+router.get('/product/create', controllers.create)
 router.get('/update/:id',controllers.update)
-router.get('/products/:id',controllers.show)
+router.get('/product/:id',controllers.show)
 
 router.post('/',[upload.any()], controllers.save) 
 
