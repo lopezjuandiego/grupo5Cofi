9542ef0723570cfad561121ed303d02e29441937
JuanMaPereyraV <juanmapereyrav@gmail.com>
2022-03-18 22:52:12 -0300

    validacion front del login 18/03 juanma

--- public/js/login.js
+++ public/js/login.js
@@ -3,52 +3,59 @@ window.onload = function() {
     const loginForm = document.forms.login;
     const fieldEmail = loginForm.email;
     const fieldPassword = loginForm.password;
+    
+    
 
+    fieldEmail.addEventListener ("keydown",  (e) =>{
+        let emailValido = fieldEmail.value.trim();
+        
 
-
-        fieldEmail.addEventListener ("keydown",  (e) =>{
-            fieldEmail.classList.remove ("error");
-            fieldEmail.classList.remove ("success");
-            fieldEmail.classList.add ("focus");
-            let value = e.target.value
-            console.log(value);
-            if (value.length < 1 ){
-                fieldEmail.classList.add ("error")
-                
-            }
-            let regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
-            if (!regex.test(value)){
-                fieldEmail.classList.add ("error")
+        
+        if (emailValido.length < 1 ){
+            fieldEmail.classList.add ("error")
+            fieldEmail.classList.remove ("succes")
             
-        } else{ 
-            fieldEmail.classList.add ("success")
-        } 
-        loginForm.addEventListener ("submit", function (e){
-            e.preventDefault()
-    })
-     })
-
-    fieldPassword.addEventListener ("keydown",  (e) =>{
-        fieldPassword.classList.remove ("error");
-        fieldPassword.classList.remove ("success");
-        fieldPassword.classList.add ("focus");
-        let value = e.target.value
-        console.log(value);
-        if (value.length < 1 ){
-            fieldPassword.classList.add ("error")
         }
-        let regex = /^.{8,50}$/
-        if (!regex.test(value)){
-            fieldPassword.classList.add ("error")
+        let regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+        if (!regex.test(emailValido)){
+            fieldEmail.classList.add ("error")
+            fieldEmail.classList.remove ("succes")
         
     } else{ 
-        fieldPassword.classList.add ("success")
-    }
+        fieldEmail.classList.add ("success")
+        
+    } 
+    
+    
+ })
+ fieldPassword.addEventListener ("keydown",  (e) =>{
+    let passwordValido = fieldPassword.value.trim();
+    
 
     
+    if (passwordValido.length < 1 ){
+        fieldPassword.classList.add ("error")
+        fieldPassword.classList.remove ("succes")
+        
+    }
+    let regex = /(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,}$/
+    if (!regex.test(passwordValido)){
+        fieldPassword.classList.add ("error")
+        fieldPassword.classList.remove ("succes")
+    
+} else{ 
+    fieldPassword.classList.add ("success")
+    
+} 
+
+
 })
- 
- }          
+
+
+
+}          
+
+
 
 
 
